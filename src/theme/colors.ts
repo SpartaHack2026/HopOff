26f88629a2e87d3b31e37d947b393d119c659899
bange1cm <bange1cm@cmich.edu>
2026-01-31 17:01:17 -0500

    adding theme colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  background: '#FEE4EB ',
+  textPrimary: '#000000',
+  textSecondary: '#ffffff',
+  lightPink: '#FEC9D7',
+  mediumPink: '#FF91AD',
+  darkPink: '#FF5883',
+  lightGreen: '#B9EEE1',
+  mediumGreen: '#79D3BE',
+  darkGreen: '#39B89A',
+};
